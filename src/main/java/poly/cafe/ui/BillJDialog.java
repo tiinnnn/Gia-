933e7e591d6a9babe749960e22b8cf73d494ecee
tiinnnn <imdino913@gmail.com>
2025-05-09 22:28:47 +0700

    test2

--- src/main/java/poly/cafe/ui/BillJDialog.java
+++ src/main/java/poly/cafe/ui/BillJDialog.java
@@ -33,11 +33,11 @@ public class BillJDialog extends javax.swing.JDialog {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 711, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 488, Short.MAX_VALUE)
         );
 
         pack();
